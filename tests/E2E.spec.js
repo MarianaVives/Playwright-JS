77cdf70eb073804aca919d6a2a1c62f4ae9c9247
MarianaVives <marianavives123@gmail.com>
2026-02-09 16:26:21 +0000

    Update project -  add E2E class part 2

--- tests/E2E.spec.js
+++ tests/E2E.spec.js
@@ -1,7 +1,7 @@
 //Documentation: https://playwright.dev/
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test.only("E2E shop products happy path", async({page})=>{
+test.only("E2E shop products happy path", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/client/auth/login");
     await expect(page).toHaveTitle("Let's Shop");
     let email = "marianavivess@hotmail.com";
@@ -19,15 +19,16 @@ test.only("E2E shop products happy path", async({page})=>{
     await pass_loc.fill(pass);
     await signin_btn.click();
 
-    await expect(shop_page_results_title).toContainText("Showing 5 results")
+    await expect(shop_page_results_title).toContainText("Showing")
     await page.waitForLoadState('networkidle'); //Discouraged - might be flaky
     await page.locator(".card-body b").first().waitFor();
     const titles = await page.locator(".card-body b").allTextContents();
-    console.log(titles); 
+    console.log(titles);
     const count = await products_list.count();
-    for(let i=0; i<count; ++i){
-        if(await products_list.nth(i).locator("b").textContent() == product_name){
+    for (let i = 0; i < count; ++i) {
+        if (await products_list.nth(i).locator("b").textContent() == product_name) {
             console.log(await products_list.nth(i).locator("b").textContent());
+            //Add to cart
             await products_list.nth(i).locator("text=Add To Cart").click();
             break;
         }
@@ -37,7 +38,43 @@ test.only("E2E shop products happy path", async({page})=>{
     await page.locator("div li").first().waitFor(); // Is visible does not support this to fail. so we use waitFor
     const isVisible = await page.locator("h3:has-text('Zara Coat 3')").isVisible(); //pseudo class from playwrigt
     expect(isVisible).toBeTruthy();
+    await page.locator("li button[type='button']").click();
+    await page.locator("div[class='field'] input[class='input txt text-validated']").fill("4542 9931 9292 2293");
+    await page.locator("div[class='field small'] select[class='input ddl']").first().selectText("01");
+    await page.locator("div[class='field small'] select[class='input ddl']").last().selectText("30");
+    await page.locator("div[class='title']:has(span[class='numberCircle']) +input").fill("852");
+    await page.locator("div[class='field'] input[class='input txt']").fill("Mariana Islandia");
+    await page.locator(".user__name label").textContent();
+    await expect(page.locator(".user__name label")).toHaveText(email);
+    await page.locator("[placeholder*='Country']").pressSequentially("united");
+    const dropdown = await page.locator("[class*='results']")
+    await dropdown.waitFor();
+    const options_count = await dropdown.locator("button").count();
+    for (let i = 0; i < options_count; ++i) {
+        const country_option = await dropdown.locator("button").nth(i).textContent();
+        if (country_option === " United Kingdom") {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+    await page.locator("a[class*='submit']").click();
+    page.locator(".hero-primary").waitFor();
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+    const order_id = await page.locator("tr[class='ng-star-inserted'] td[class='em-spacer-1'] label").textContent();
+    console.log(order_id)
+    await page.locator("button[routerlink*='myorders']").click();
 
-    //await page.pause()
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+
+    for (let i = 0; i < await rows.count(); ++i) {
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if (order_id.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(order_id.includes(orderIdDetails)).toBeTruthy();
 
 })
